0698bafdced75de5ce31637a3e79e43c82f826e2
okoni <ky0329ky3776ky@icloud.com>
2024-06-15 09:27:55 +0900

    Create Hello component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
 
+import Hello from "../components/Hello"
+
 
 const App = (): JSX.Element =>{
 return(
     <View style={styles.container}>
+        <Hello bang={true}>World</Hello>
+        <Hello style={{ fontSize:16 }} bang={true}>World</Hello>
         <Text>test dest</Text>
         <StatusBar style="auto" />
     </View>
---
+++ src/components/Hello.tsx
@@ -0,0 +1,31 @@
+import { View, Text, StyleSheet, type TextStyle } from "react-native";
+
+interface Props {
+    children: string //Hello関数のchildrenの型を定義する
+    bang?: boolean
+    style?: TextStyle
+}
+
+const Hello = (props: Props): JSX.Element => {
+    const { children, bang, style} = props
+    return(
+        <View>
+            <Text style={[styles.text,style]}>
+                Hello {children}{bang === true ? "!" : ""}
+            </Text>
+        </View>
+    )
+}
+
+// スタイルシートの変更
+const styles = StyleSheet.create({
+    text: {
+        color:'#ffffff',
+        backgroundColor:'blue',
+        fontSize:40,
+        fontWeight:'bold',
+        padding: 16,
+    }
+})
+
+export default Hello
