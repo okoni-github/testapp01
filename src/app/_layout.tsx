3466942a09ae04e15a543b8f158bf00e8bc81d8b
okoni <ky0329ky3776ky@icloud.com>
2024-06-16 10:15:56 +0900

    Install expo-router

---
+++ src/app/_layout.tsx
@@ -0,0 +1,8 @@
+//テンプレート
+import { Slot } from 'expo-router'
+
+const Layout = ():JSX.Element => {
+　return <Slot />
+}
+
+export default Layout
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,27 +1,12 @@
-import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
-
-import Hello from "../components/Hello"
+import {  View, Text } from "react-native";
 
 
-const App = (): JSX.Element =>{
-return(
-    <View style={styles.container}>
-        <Hello bang={true}>World</Hello>
-        <Hello style={{ fontSize:16 }} bang={true}>World</Hello>
-        <Text>test dest</Text>
-        <StatusBar style="auto" />
+const Index = ():JSX.Element => {
+  return(
+    <View>
+      <Text>Hello World</Text>
     </View>
-)
-}
+  )
+} 
 
-const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor: '#fff',
-        alignItems: "center",
-        justifyContent: "center"
-    }
-})
-
-export default App
+export default Index
