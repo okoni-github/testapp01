fb9a7af44e65582d0918afa22d0ce969a9d910b7
okoni <ky0329ky3776ky@icloud.com>
2024-07-06 14:15:54 +0900

    database update

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -3,22 +3,43 @@ import {
     View, TextInput, StyleSheet, KeyboardAvoidingView
 } from "react-native";
 import { router } from 'expo-router';
+import { collection, addDoc, Timestamp } from 'firebase/firestore';
+import { useState } from 'react';
 
 import CircleButton from '@/src/components/circleButton';
 import Icon from '@/src/components/icon';
+import { db, auth } from '../../config'
 
-const handlePress = (): void => {
-    router.back()
+const handlePress = (bodyText: string): void => {
+    if (auth.currentUser === null) { return }
+    const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+    addDoc(ref, {
+        bodyText,
+        updatedAt: Timestamp.fromDate(new Date())
+    })
+        .then((docRef) => {
+            console.log('success!', docRef.id)
+            router.back()
+        })
+        .catch((error) => {
+            console.log(error)
+        })
 }
 
 const Create = ():JSX.Element => {
+    const [bodyText, setBodyText] = useState('')
     return(
         //キーボードが上がってくると画面が縮小される
         <KeyboardAvoidingView behavior='height' style={styles.container}>
             <View style={styles.inputContainer}>
-                <TextInput multiline style={styles.input} value=''/>
+                <TextInput
+                    multiline
+                    style={styles.input}
+                    value={bodyText}
+                    onChangeText={(text) => { setBodyText(text) }}
+                />
             </View>
-            <CircleButton onPress={handlePress}>
+            <CircleButton onPress={() => { handlePress(bodyText) }}>
                 <Icon name='check' size={40} color='#ffffff'/>
             </CircleButton>
         </KeyboardAvoidingView>
